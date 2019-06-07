87545dec1db72a38e05d181c25941f749893bc05
Roger-luo <hiroger@qq.com>
2019-06-07 12:28:06 -0400

    update deps & rm cuda

--- src/BatchedRoutines.jl
+++ src/BatchedRoutines.jl
@@ -1,19 +1,7 @@
 module BatchedRoutines
 
-macro __PKGNAME__()
-    return "BatchedRoutines"
-end
-
-const ext = joinpath(dirname(@__DIR__), "deps", "ext.jl")
-isfile(ext) || error("$(@__PKGNAME__).jl has not been built, please run Pkg.build(\"$(@__PKGNAME__)\").")
-include(ext)
-
 include("blas.jl")
 include("lapack.jl")
 include("linalg.jl")
-
-@static if USE_CUDA
-    include("cuda/cuda.jl")
-end
 
 end # module
